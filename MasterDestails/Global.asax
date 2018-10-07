511fe01d684990771a46c341950b8a6b7b964507
muhamed ibrahim <mohamed77ebrahim@yahoo.com>
2018-10-07 14:35:56 +0200

    Add project files.

---
+++ MasterDestails/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MasterDestails.MvcApplication" Language="C#" %>
---
+++ MasterDestails/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MasterDestails.Startup))]
+namespace MasterDestails
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
