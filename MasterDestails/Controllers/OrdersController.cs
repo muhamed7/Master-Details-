8b32418e6c87c73279937bffa5934c965ff8dcdb
muhamed ibrahim <mohamed77ebrahim@yahoo.com>
2018-11-14 15:45:47 +0200

    first commit

---
+++ MasterDestails/Controllers/OrdersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasterDestails.Models;
+
+namespace MasterDestails.Controllers
+{
+    public class OrdersController : Controller
+    {
+        OnlineShopEntities db = new OnlineShopEntities();
+
+        public ActionResult Index()
+        {
+            List<Customer> OrderAndCustomerList = db.Customers.ToList();
+            
+            return View(OrderAndCustomerList);
+        }
+        public ActionResult SaveOrder(string name, String address, Order[] order)
+        {
+
+            string result = "Error! Order Is Not Complete!";
+            if (name != null && address != null && order != null)
+            {
+                var cutomerId = Guid.NewGuid();
+                Customer model = new Customer();
+                model.CustomerId = cutomerId;
+                model.Name = name;
+                model.Address = address;
+                model.OrderDate = DateTime.Now;
+                db.Customers.Add(model);
+
+
+                foreach (var item in order)
+                {
+                    var orderId = Guid.NewGuid();
+                    Order O = new Order();
+                    O.OrderId = orderId;
+                    O.ProductName = item.ProductName;
+                    O.Quantity = item.Quantity;
+                    O.Price = item.Price;
+                    O.Amount = item.Amount;
+                    O.CustomerId = cutomerId;
+                    db.Orders.Add(O);
+                }
+                db.SaveChanges();
+                result = "Success! Order Is Complete!";
+            }
+            return Json(result, JsonRequestBehavior.AllowGet);
+
+        }
+    }
+}
